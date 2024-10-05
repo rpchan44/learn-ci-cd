ea98e7d2b465949e29f7b7a7c390a578242a4d4d
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-05 11:18:05 +0800

    Refactor php code

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 require 'src/loan.class.php';
 $loan = new loan;
 $total = $loan->computeloan(500);
-print "You loan 500 pesos and you are about to pay $total with interest";
+print "<center><h1>You loan 500 pesos and you are about to pay $total with 20% interest</h1></center>";
 ?>
