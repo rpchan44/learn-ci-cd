ba4809f76b4c7656a8261958d43ef1a51d031c52
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-05 12:17:26 +0800

    Fix

--- process.php
+++ process.php
@@ -2,12 +2,11 @@
 require 'src/loan.class.php';
 $loan = new loan;
 
-if (!isset($_POST['amount'])) {
+if (isset($_POST['amount'])) {
 
-$amount = $_POST['amount'];
-$total = $loan->computeloan($amount);
-print "<center><h1>You loan $total pesos and you are about to pay $total with 20% interest</h1></center>";
-
+	$amount = $_POST['amount'];
+	$total = $loan->computeloan($amount);
+	print "<center><h1>You loan $total pesos and you are about to pay $total with 20% interest</h1></center>";
 } else {
         header("location: /");
 }
