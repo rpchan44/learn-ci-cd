f919693dd34bd1443133c4e0d739323fa026448d
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-05 20:26:06 +0800

    Update process.php update formatting

--- src/process.php
+++ src/process.php
@@ -12,7 +12,6 @@ if ( isset($_POST['amount']) && (int)isset($_POST['amount']) > 0 ) {
 	$monthly = number_format($total / 12, 2, '.', '');
 	print "<p><center><h1>Thank you! $firstname $lastname</h1><center></br>";
 	print "Principal amount is $amount to be amortize + 20% interest is $total</br>";
-	print "Principal amount is $amount to be amortize + 20% interest is $total</br>";
 	print "Your monthly dues per month is $monthly within a year!</center></p>";
 } else {
         header("location: /");
