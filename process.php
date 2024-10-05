169aa684d9a2f3d3e05f0aaa671b5a18c36f3824
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-05 12:09:34 +0800

    refactor process.php

--- process.php
+++ process.php
@@ -1,7 +1,13 @@
 <?php
 require 'src/loan.class.php';
 $loan = new loan;
-$amount = $_POST['amount'];
+
+if (isset($amount = $_POST['amount'])) {
 $total = $loan->computeloan($amount);
 print "<center><h1>You loan $total pesos and you are about to pay $total with 20% interest</h1></center>";
+
+} else { 
+	header("location: /");
+}
+
 ?>
