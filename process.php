0088c924fbb34de3aee5fe38eb6e274a48d58842
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-05 17:13:40 +0800

    Update process.php

--- process.php
+++ process.php
@@ -11,7 +11,7 @@ if ( isset($_POST['amount']) && (int)isset($_POST['amount']) > 0 ) {
 	$monthly = $total / 12;
 	print "<center><h1>Mr/Ms $firstname $lastname</h1><center></br>";
 	print "<center>Principal amount is $amount to be amortize + 20% interest is $total</center></br>";
-	print "<center>Your monthly dues per month is $monthly / year</center>";
+	print "<center>Your monthly dues per month is $monthly within a year!</center>";
 } else {
         header("location: /");
 }
