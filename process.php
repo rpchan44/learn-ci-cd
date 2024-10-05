7c571a0554c6ea11d3162b460add917ec7add7b3
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-05 17:42:39 +0800

    Update process.php

--- process.php
+++ process.php
@@ -6,7 +6,8 @@ if ( isset($_POST['amount']) && (int)isset($_POST['amount']) > 0 ) {
 	$amount = (float)filter_var($_POST['amount'], FILTER_SANITIZE_STRING);
 	$firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_STRING);
 	$lastname = filter_var($_POST['lastname'], FILTER_SANITIZE_STRING);
-	$total = number_format($total, 2, ',', ' ');
+	$computation = $loan->computeloan($amount);
+	$total = number_format($computation, 2, ',', ' ');
 	$monthly = $total / 12;
 	print "<center><h1>Mr/Ms $firstname $lastname</h1><center></br>";
 	print "<center>Principal amount is $amount to be amortize + 20% interest is $total</center></br>";
