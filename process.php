115d9e337880a676bc3275ef11ffb9d8df12bc35
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-05 17:01:50 +0800

    Update process.php

--- process.php
+++ process.php
@@ -9,7 +9,7 @@ if ( isset($_POST['amount']) && (int)isset($_POST['amount']) > 0 ) {
 	$lastname = strip_tags($_POST['lastname']);
 	$total = $loan->computeloan($amount);
 	$monthly = $total / 12;
-	print "<center><h1>Welcome $firstname, $lastname</h1><center></br>";
+	print "<center><h1>Mr/Ms $firstname $lastname</h1><center></br>";
 	print "<center><h1>Your total loan + 20% interest is $total your monthly dues in a year is $monthly/</h1></center>";
 } else {
         header("location: /");
