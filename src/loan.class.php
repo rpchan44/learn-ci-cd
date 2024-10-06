d0ee8735a0c48601e0f1da67f69bf8d8de5d685e
Ronaldo Chan <ronaldopchan@outlook.com>
2024-10-06 14:26:05 +0800

    Update and fix regression

--- src/loan.class.php
+++ src/loan.class.php
@@ -3,7 +3,7 @@
 class loan {
 
         public function computeloan(float $amount): float {
-                return $amount = $amount + ($amount * 0.10);
+                return $amount = $amount + ($amount * 0.20);
         }
 }
 ?>
